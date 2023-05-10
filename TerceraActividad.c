22b0c0e74f0914f6223c0f2f6d7de0c03dfdba58
nicolass00 <mati.nico.fernandez@gmail.com>
2023-05-10 18:55:13 -0300

    Update TerceraActividad.c

--- TerceraActividad.c
+++ TerceraActividad.c
@@ -2,27 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 int n;  // dato
-bool esPrimo; //variable intermedia
+int esPrimo; //variable intermedia
 int sqrtN; //variable intermedia
 int i; //contador
-char primo;  // resultado
+char primo[11];  // resultado
 int main()
 {
     printf("\n Ingrese un numero: ");
     scanf("%i", &n);
-    esPrimo = true;
-    if(n%2 == 0) {
-        esPrimo = false;
+    esPrimo = 1;
+    if(n%2 == 0 && n != 2) {
+        esPrimo = 0;
     }
     
     sqrtN = (int)sqrt(n);
     i = 1;
-    while(esPrimo && i <= sqrtN) {
+    while(esPrimo == 1 && i <= sqrtN) {
         i += 2;
         if(n%i == 0) {
-            esPrimo = false;
+            esPrimo = 0;
         }
     }
+    
+    if(esPrimo == 1) {
+        strcpy(primo, "Es primo");
+    }
+    else {
+        strcpy(primo, "No es primo");
+    }
+    
+    printf("%s", primo);
     
     return 0;
 }
