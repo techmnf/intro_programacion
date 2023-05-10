33662a8242d4bf4be0b3be0a001633504bbb1a3d
nicolass00 <mati.nico.fernandez@gmail.com>
2023-05-10 09:04:15 -0300

    Update TerceraEntrega.c

--- TerceraEntrega.c
+++ TerceraEntrega.c
@@ -1,7 +1,10 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 int n;  // dato
 bool esPrimo; //variable intermedia
+int sqrtN; //variable intermedia
+int i; //contador
 char primo;  // resultado
 int main()
 {
@@ -12,8 +15,13 @@ int main()
         esPrimo = false;
     }
     
-    while(esPrimo) {
-      
+    sqrtN = (int)sqrt(n);
+    i = 1;
+    while(esPrimo && i <= sqrtN) {
+        i += 2;
+        if(n%i == 0) {
+            esPrimo = false;
+        }
     }
     
     return 0;
