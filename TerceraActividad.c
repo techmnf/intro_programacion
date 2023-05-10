95b34e7a52f6a59dd5897e715e500b416f407bc5
nicolass00 <mati.nico.fernandez@gmail.com>
2023-05-10 18:48:04 -0300

    Update TerceraActividad.c

--- TerceraActividad.c
+++ TerceraActividad.c
@@ -11,7 +11,7 @@ int main()
     printf("\n Ingrese un numero: ");
     scanf("%i", &n);
     esPrimo = true;
-    if(n%2 == 0 && n != 2) {
+    if(n%2 == 0) {
         esPrimo = false;
     }
     
