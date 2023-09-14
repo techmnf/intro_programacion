f4afec8c9363d5ec8f688238138fbde5924ae57f
nicolass00 <mati.nico.fernandez@gmail.com>
2023-09-13 22:20:11 -0300

    Update Practico14.c

--- Practicos/Practico14.c
+++ Practicos/Practico14.c
@@ -30,37 +30,41 @@ void MuestraRegistro(TElem *list, int pos);
 void CargarSocio(TSocio *reg);
 void LiberarMemoria(TElem **list);
 
+void MuestraReg(TSocio reg);
+
 int main() {
   Inicializar(&q);
   do {
     printf("Ingrese una opción: ");
-    printf("\t 1.Insertar a la cabeza");
-    printf("\t 2.Buscar un elemento");
-    printf("\t 3.Mostrar un registro");
-    printf("\t 4.Mostrar todos los registros");
-    printf("\t 5.Vaciar la lista");
-    printf("\t 6.Salir");
+    printf("\n\t 1.Insertar a la cabeza");
+    printf("\n\t 2.Buscar un elemento");
+    printf("\n\t 3.Mostrar un registro");
+    printf("\n\t 4.Mostrar todos los registros");
+    printf("\n\t 5.Vaciar la lista");
+    printf("\n\t 6.Salir");
     
+    printf("\n Elija una opcion: ");
+
     scanf("%i", &opcion);
     switch(opcion) {
       case 1:
         CargarSocio(&socio);
         InsertarC(q, socio);
         break;
-      
+
       case 2:
-        printf("\t Ingrese el nombre del socio a buscar");
+        printf("\t Ingrese el nombre del socio a buscar: ");
         scanf("%s", nombre);
         posicion = Buscar(q, nombre);
-        
+
         printf("La posicion es: %d", posicion);
         break;
-      
+
       case 3:
-        printf("\t Ingrese el nombre del socio a buscar");
+        printf("\t Ingrese el nombre del socio a buscar: ");
         scanf("%s", nombre);
         posicion = Buscar(q, nombre);
-        
+
         if(posicion == -1) {
           printf("\n No se encuentra el nombre de ese socio");
         }
@@ -68,17 +72,19 @@ int main() {
           MuestraRegistro(q, posicion);
         }
         break;
-      
+
       case 4:
         Mostrar(q);
         break;
-      
+
       case 5:
         LiberarMemoria(&q);
         break;
     }
     
-  } while(opcion == 6);
+    printf("\n");
+
+  } while(opcion != 6);
   return 0;
 }
 
@@ -99,11 +105,11 @@ int Buscar(TElem *list, char nombreBuscado[100]) {
   int pos = 0;
   TElem *aux = list->next;
   while(aux != NULL) {
-    pos++;
     if(strcmp((aux->info).nombre, nombreBuscado) == 0) {
       return pos;
     }
     
+    pos++;
     aux = aux->next;
   }
   return -1;
@@ -112,29 +118,7 @@ int Buscar(TElem *list, char nombreBuscado[100]) {
 void Mostrar(TElem *list) {
   TElem *aux = list->next;
   while(aux != NULL) {
-    printf("\t Nombre: ");
-    printf("%s", aux->info.nombre);
-    
-    printf("\t Apellido: ");
-    printf("%s", aux->info.apellido);
-    
-    printf("\t Telefono: ");
-    printf("%s", aux->info.telefono);
-    
-    printf("\t Calle Domicilio: ");
-    printf("%s", aux->info.calleDomicilio);
-    
-    printf("\t Edad: ");
-    printf("%d", aux->info.edad);
-    
-    printf("\t Sexo: ");
-    if(aux->info.sexo == 0) {
-      printf("F");
-    }
-    else {
-      printf("M");
-    }
-    
+    MuestraReg(aux->info);
     aux = aux->next;
   }
 }
@@ -154,80 +138,36 @@ void MuestraRegistro(TElem *list, int pos) {
         aux = aux->next;
         cont++;
       }
-      
+
       if(aux != NULL) {
-        printf("\t Nombre: ");
-        printf("%s", aux->info.nombre);
-        
-        printf("\t Apellido: ");
-        printf("%s", aux->info.apellido);
-        
-        printf("\t Telefono: ");
-        printf("%s", aux->info.telefono);
-        
-        printf("\t Calle Domicilio: ");
-        printf("%s", aux->info.calleDomicilio);
-        
-        printf("\t Edad: ");
-        printf("%d", aux->info.edad);
-        
-        printf("\t Sexo: ");
-        if(aux->info.sexo == 0) {
-          printf("F");
-        }
-        else {
-          printf("M");
-        }
+        MuestraReg(aux->info);
       }
     }
   }
 }
 
 void CargarSocio(TSocio *reg) {
-  printf("Ingrese los datos de un Socio");
-  
-  printf("Ingrese el nombre del socio");
+  printf("Ingrese los datos de un Socio: \n");
+
+  printf("Ingrese el nombre del socio: ");
   scanf("%s", reg->nombre);
-  
-  printf("Ingrese el apellido del socio");
+
+  printf("Ingrese el apellido del socio: ");
   scanf("%s", reg->apellido);
-  
-  printf("Ingrese el número de telefono del socio");
+
+  printf("Ingrese el número de telefono del socio: ");
   scanf("%s", reg->telefono);
-  
-  printf("Ingrese la calle del Domicilio del socio");
+
+  printf("Ingrese la calle del Domicilio del socio: ");
   scanf("%s", reg->calleDomicilio);
-  
-  printf("Ingrese la edad del socio");
+
+  printf("Ingrese la edad del socio: ");
   scanf("%i", &reg->edad);
-  
-  printf("Ingrese el sexo (F o M) del socio");
+
+  printf("Ingrese el sexo (F o M) del socio: ");
   scanf("%i", &reg->sexo);
-  
-  
-  
-  printf("\t Nombre: ");
-  printf("%s", reg->nombre);
-        
-  printf("\t Apellido: ");
-  printf("%s", reg->apellido);
-        
-  printf("\t Telefono: ");
-  printf("%s", reg->telefono);
-        
-  printf("\t Calle Domicilio: ");
-  printf("%s", reg->calleDomicilio);
-        
-  printf("\t Edad: ");
-  printf("%d", reg->edad);
-        
-  printf("\t Sexo: ");
-  if(reg->sexo == 0) {
-    printf("F");
-  }
-  else {
-    printf("M");
-  }
+
+  MuestraReg(*reg);
 }
 
 void LiberarMemoria(TElem **list) {
@@ -237,4 +177,29 @@ void LiberarMemoria(TElem **list) {
     (*list)->next = aux->next;
     free(aux);
   }
+}
+
+void MuestraReg(TSocio reg) {
+    printf("\n\t Nombre: ");
+    printf("%s", reg.nombre);
+    
+    printf("\n\t Apellido: ");
+    printf("%s", reg.apellido);
+    
+    printf("\n\t Telefono: ");
+    printf("%s", reg.telefono);
+    
+    printf("\n\t Calle Domicilio: ");
+    printf("%s", reg.calleDomicilio);
+    
+    printf("\n\t Edad: ");
+    printf("%d", reg.edad);
+    
+    printf("\n\t Sexo: ");
+    if(reg.sexo == 0) {
+        printf("F");
+    }
+    else {
+        printf("M");
+    }
 }
